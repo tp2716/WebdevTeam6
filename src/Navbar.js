ecd3929ed3000563a6314a1339fa813220a2ff00
Glitch (hexagonal-boggy-jingle) <none>
2023-11-20 03:18:48 +0000

    adjusted some styles and added href to account in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,9 @@ export default function renderNavbar() {
           <div class="col-md-4">
             <header>
               <ul>
-                <li><a href="index.html#about">HOME</a></li>
+                <li><a href="index.html">HOME</a></li>
                 <li><a href="explore.html">LEARN</a></li>
-                <li><a href="index.html#projects">ACCOUNT</a></li>
+                <li><a href="account.html">ACCOUNT</a></li>
               </ul>
             </header>
           </div>`;
