92bb614292179174404f5f643f47bb984c61d8bc
You Peng <52520465+tp2716@users.noreply.github.com>
2024-09-10 20:17:46 -0400

    New Proxy to get search working

--- src/request.js
+++ src/request.js
@@ -1,5 +1,5 @@
 export default function makeCorsRequest(query) {
-  let proxy = 'https://corsproxy.io/?';
+  let proxy = 'https://thingproxy.freeboard.io/fetch/';
   //return fetch(proxy+'https://api.edamam.com/api/recipes/v2/ad8ecf532b114c7a12eae0543b99dcb4?type=public&app_id=47a14aa1&app_key=5f6e4fae1531f227d8a74ee073ef6c6f', {
   return fetch(proxy+'https://api.edamam.com/api/recipes/v2?type=any&app_id=47a14aa1&app_key=5f6e4fae1531f227d8a74ee073ef6c6f&q=' + query + '&random=true', {
     mode:  'cors',
